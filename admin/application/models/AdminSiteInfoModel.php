4bdf1f61fd575e4c640c37fa2dc2be53d6f276ee
Ari Putra <ariteknologi@gmail.com>
2020-04-11 17:18:59 +0700

    fix minor

--- admin/application/models/AdminSiteInfoModel.php
+++ admin/application/models/AdminSiteInfoModel.php
@@ -15,7 +15,7 @@ class AdminSiteInfoModel extends CI_Model
     
     public function getSiteInfo($id)
     {
-        $sql = "SELECT * FROM siteinfo where siteinfo_id =".$id;
+        $sql = "SELECT * FROM siteinfo where siteinfo_id = ".$id;
         $querysql =$this->db->query($sql);
 
         return $querysql->result_array();
