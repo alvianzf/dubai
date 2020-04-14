9451b42392bc6025081a472b3c29c2b194e8b08d
Ari Putra <ariteknologi@gmail.com>
2020-04-14 16:18:11 +0700

    fix time time

--- application/views/result_content.php
+++ application/views/result_content.php
@@ -29,59 +29,81 @@
 												</tr>
 											</thead>
 											<tbody>
-												<?php
+												<?php 
+													foreach ($product_data_result as $data_1st) {
+														$date=date_create($data_1st['tanggal']);
+												?>
+													<?php if(date_format($date,"d") == date("d") && strtotime(date("H:i:s")) > strtotime($show_time['preparation_time']) ) { ?>
+														<?php if(date_format($date,"d") == date("d") && strtotime(date("H:i:s")) < strtotime($show_time['first_result_time']) ) { ?>
 
-												foreach ($product_data_result as $data_1st_result) {
-													$date=date_create($data_1st_result['tanggal']);
-													?>
-														<?php if(date_format($date,"d") == date("d") && strtotime(date("H:i:s")) < strtotime($show_time['first_result_time']) && strtotime(date("H:i:s")) < strtotime($show_time['preparation_time'])) { ?>
 
-															<?php } elseif(date_format($date,"d") == date("d") && strtotime(date("H:i:s")) > strtotime($show_time['first_result_time']) && strtotime(date("H:i:s")) > strtotime($show_time['preparation_time']) ){ ?>
-																<tr>
-																	<td>
-																	<?php echo $data_1st_result['hasil_id']; ?>
-																	</td>
-																	<td class="td_mobile">
-																		<span style="text-transform:capitalize;"><?php echo $data_1st_result['hari']; ?></span>
-																	</td>
-																	<td class="td_date">
-																		<?php 
-																			
-																			echo date_format($date,"d-m-Y");?> 	
-																	</td>
-																	<td>
-																		<div class="result_page" style="text-align:center;">
-																			<span class="ball_result_page"><?php $data13=$data_1st_result['hasil_1'];echo $namafile= substr($data13,0,-3);?></span>
-																			<span class="ball_result_page"><?php $data13=$data_1st_result['hasil_1'];echo $namafile= substr($data13,1,-2);?></span>
-																			<span class="ball_result_page"><?php $data13=$data_1st_result['hasil_1'];echo $namafile= substr($data13,2,-1);?></span>
-																			<span class="ball_result_page"><?php $data13=$data_1st_result['hasil_1'];echo $namafile= substr($data13,3,1);?></span>
-																		</div>
-																	</td>
-																</tr>
-															<?php } else { ?>
-																<tr>
-																	<td>
-																	<?php echo $data_1st_result['hasil_id']; ?>
-																	</td>
-																	<td class="td_mobile">
-																		<span style="text-transform:capitalize;"><?php echo $data_1st_result['hari']; ?></span>
-																	</td>
-																	<td class="td_date">
-																		<?php 
-																			echo date_format($date,"d-m-Y");?> 	
-																	</td>
-																	<td>
-																		<div class="result_page" style="text-align:center;">
-																			<span class="ball_result_page"><?php $data13=$data_1st_result['hasil_1'];echo $namafile= substr($data13,0,-3);?></span>
-																			<span class="ball_result_page"><?php $data13=$data_1st_result['hasil_1'];echo $namafile= substr($data13,1,-2);?></span>
-																			<span class="ball_result_page"><?php $data13=$data_1st_result['hasil_1'];echo $namafile= substr($data13,2,-1);?></span>
-																			<span class="ball_result_page"><?php $data13=$data_1st_result['hasil_1'];echo $namafile= substr($data13,3,1);?></span>
-																		</div>
-																	</td>
-																</tr>
-															<?php } ?>
-													<?php }  ?>
-																
+														<?php } elseif(date_format($date,"d") == date("d") && strtotime(date("H:i:s")) > strtotime($show_time['first_result_time']) ){ ?>
+															<tr>
+																<td>
+																<?php echo $data_1st['hasil_id']; ?>
+																</td>
+																<td class="td_mobile">
+																	<span style="text-transform:capitalize;"><?php echo $data_1st['hari']; ?></span>
+																</td>
+																<td class="td_date">
+																	<?php 
+																		
+																		echo date_format($date,"d-m-Y");?> 	
+																</td>
+																<td>
+																	<div class="result_page" style="text-align:center;">
+																		<span class="ball_result_page"><?php $data1=$data_1st['hasil_1'];echo $namafile= substr($data1,0,-3);?></span>
+																		<span class="ball_result_page"><?php $data1=$data_1st['hasil_1'];echo $namafile= substr($data1,1,-2);?></span>
+																		<span class="ball_result_page"><?php $data1=$data_1st['hasil_1'];echo $namafile= substr($data1,2,-1);?></span>
+																		<span class="ball_result_page"><?php $data1=$data_1st['hasil_1'];echo $namafile= substr($data1,3,1);?></span>
+																	</div>
+																</td>
+															</tr>
+														<?php } else { ?>
+															<tr>
+																<td>
+																<?php echo $data_1st['hasil_id']; ?>
+																</td>
+																<td class="td_mobile">
+																	<span style="text-transform:capitalize;"><?php echo $data_1st['hari']; ?></span>
+																</td>
+																<td class="td_date">
+																	<?php 
+																		echo date_format($date,"d-m-Y");?> 	
+																</td>
+																<td>
+																	<div class="result_page" style="text-align:center;">
+																		<span class="ball_result_page"><?php $data1=$data_1st['hasil_1'];echo $namafile= substr($data1,0,-3);?></span>
+																		<span class="ball_result_page"><?php $data1=$data_1st['hasil_1'];echo $namafile= substr($data1,1,-2);?></span>
+																		<span class="ball_result_page"><?php $data1=$data_1st['hasil_1'];echo $namafile= substr($data1,2,-1);?></span>
+																		<span class="ball_result_page"><?php $data1=$data_1st['hasil_1'];echo $namafile= substr($data1,3,1);?></span>
+																	</div>
+																</td>
+															</tr>
+														<?php } ?>
+													<?php } else { ?>
+														<tr>
+																<td>
+																<?php echo $data_1st['hasil_id']; ?>
+																</td>
+																<td class="td_mobile">
+																	<span style="text-transform:capitalize;"><?php echo $data_1st['hari']; ?></span>
+																</td>
+																<td class="td_date">
+																	<?php 
+																		echo date_format($date,"d-m-Y");?> 	
+																</td>
+																<td>
+																	<div class="result_page" style="text-align:center;">
+																		<span class="ball_result_page"><?php $data1=$data_1st['hasil_1'];echo $namafile= substr($data1,0,-3);?></span>
+																		<span class="ball_result_page"><?php $data1=$data_1st['hasil_1'];echo $namafile= substr($data1,1,-2);?></span>
+																		<span class="ball_result_page"><?php $data1=$data_1st['hasil_1'];echo $namafile= substr($data1,2,-1);?></span>
+																		<span class="ball_result_page"><?php $data1=$data_1st['hasil_1'];echo $namafile= substr($data1,3,1);?></span>
+																	</div>
+																</td>
+															</tr>
+													<?php } ?>
+												<?php } ?>
 											</tbody>
 										</table>
 									</div>
